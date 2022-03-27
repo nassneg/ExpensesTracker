c51e50cd86d70c0089da5474f1a2f0f71237ad18
nassneg <snailfail@yandex.ru>
2022-03-26 17:43:22 -0700

    Node backend setup

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const server = app.listen(process.env.PORT || 8080, () =>
+  console.log("Listening on 8080")
+);
+
+app.use(express.static("public"));
+app.use(express.json());
